79e928483d7d0bad0640854806fd904fce2f30d4
dipta995 <dipta995@gmail.com>
2022-07-05 01:47:29 +0600

    Font awesome support

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://unpkg.com/tailwindcss@1.4.6/dist/tailwind.min.css">
--- resources/views/livewire/friend-component.blade.php
+++ resources/views/livewire/friend-component.blade.php
@@ -4,17 +4,24 @@
     <div class="contacts p-2 flex-1 overflow-y-scroll">
         <div class="flex justify-between items-center p-3 hover:bg-gray-800 rounded-lg relative">
             <div class="w-16 h-16 relative flex flex-shrink-0">
-                <img class="shadow-md rounded-full w-full h-full object-cover"
+              <span class="z-20 ml-0">
+                @php
+                    $count = DB::table('messages')->where('thread',  $item->id.'-'.Auth::user()->id)->where('is_seen','0')->count();
+
+                 @endphp
+                @if ($count==0)
+                @else
+                <i class="text-red-600 fas fa-bell"></i>
+                @endif
+
+              </span>
+                     <img class="shadow-md rounded-full w-full h-full object-cover"
                     src="https://randomuser.me/api/portraits/women/61.jpg" alt="" />
+
             </div>
 
     <a href="{{ url('home') }}?userid={{ $item->id }}" class="flex-auto min-w-0 ml-4 mr-6 hidden md:block group-hover:block">
-        <p>{{ $item->name }}
-
-            @php
-                $count = DB::table('messages')->where('thread',  $item->id.'-'.Auth::user()->id)->where('is_seen','0')->count();
-                echo  $count;
-            @endphp
+        <p>{{ $item->name }} {{ $count==0 ?'':'('.$count.')' }}
 
 
         </p>
